edf8ee507995021cc35f7d597f38fdd0e83efaec
AnkitaHeralagi <126943633+AnkitaHeralagi@users.noreply.github.com>
2026-03-13 19:59:06 +0530

    Updated

--- BankProfile.cs
+++ BankProfile.cs
@@ -1,44 +1,51 @@
-﻿using System;
-class BankProfile
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array_of_Accounts_Bank_
 {
-    private string accountId;
-    private string holderName;
-    private double balance;
-    private string type;
-    public string AccountId => accountId;
-    public string HolderName => holderName;
-    public double Balance => balance;
-    public string Type => type;
-    public BankProfile(string name, double initialAmount, string accType)
+    internal class BankProfile
     {
-        holderName = name;
-        balance = initialAmount;
-        type = accType;
-        accountId = GenerateAccountNumber();
-    }
-    private string GenerateAccountNumber()
-    {
-        Random r = new Random();
-        return "IDBI" + r.Next(100000000, 999999999);
-    }
-    public void AddFunds(double amount)
-    {
-        if (amount > 0)
+        private string accountId;
+        private string holderName;
+        private double balance;
+        private string type;
+        public string AccountId => accountId;
+        public string HolderName => holderName;
+        public double Balance => balance;
+        public string Type => type;
+        public BankProfile(string name, double initialAmount, string accType)
         {
-            balance += amount;
-            Console.WriteLine("Amount deposited successfully.");
+            holderName = name;
+            balance = initialAmount;
+            type = accType;
+            accountId = GenerateAccountNumber();
         }
-    }
-    public void TakeFunds(double amount)
-    {
-        if (balance - amount >= 500)
+        private string GenerateAccountNumber()
         {
-            balance -= amount;
-            Console.WriteLine("Withdrawal successful.");
+            Random r = new Random();
+            return "IDBI" + r.Next(100000000, 999999999);
         }
-        else
+        public void AddFunds(double amount)
         {
-            Console.WriteLine("Cannot withdraw. Minimum balance of 500 must be maintained.");
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine("Amount deposited successfully");
+            }
         }
+        public void TakeFunds(double amount)
+        {
+            if (balance - amount >= 500)
+            {
+                balance -= amount;
+                Console.WriteLine("Withdrawal successful.");
+            }
+            else
+            {
+                Console.WriteLine("Cannot withdraw. Minimum balance of 500 must be maintained");
+            }
+        }
+
     }
 }
